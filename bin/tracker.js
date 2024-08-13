cfae37c89c588f1d67e50102ecb7d3e0fd63dab7
udbhavbalaji <udbhavbalaji@gmail.com>
2024-08-13 12:55:21 -0400

    Disabled the config command until correct implementation

--- bin/tracker.js
+++ bin/tracker.js
@@ -1,16 +1,19 @@
 #!/usr/bin/env node
 
 import { Command } from "commander";
+
 import { loadConfig } from "../src/config/config.js";
 import { initCommand } from "../src/commands/init.js";
 import { createCommand } from "../src/commands/create.js";
-import { configCommand } from "../src/commands/config.js"
 import { addToCommand } from "../src/commands/addTo.js";
 import { trackCommand } from "../src/commands/track.js";
-import { generateCommand } from "../src/commands/generate.js";
 import { showCommand } from "../src/commands/show.js";
 import { deleteFromCommand } from "../src/commands/deleteFrom.js";
 import { deleteCommand } from "../src/commands/delete.js";
+import { generateCommand } from "../src/commands/generate.js";
+
+// command is disabled till correct implementation
+// import { configCommand } from "../src/commands/config.js"
 
 
 const currentConfig = loadConfig();
@@ -31,8 +34,10 @@ if (Object.keys(currentConfig).length === 0) {
     program.addCommand(deleteFromCommand)
     program.addCommand(deleteCommand)
     program.addCommand(generateCommand);
+
     // todo: replace the 'config show' command with the 'show [config|any]' command to generalize command to view app's/dataset's current state
-    program.addCommand(configCommand);
+    // command is disabled till correct implementation
+    // program.addCommand(configCommand);
 }
 
 program.parse(process.argv);

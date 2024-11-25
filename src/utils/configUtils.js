c2b4034f3f7c9e46c013b45f276455fcaed2e25d
udbhavbalaji <udbhavbalaji@gmail.com>
2024-11-25 16:13:26 -0500

    Added error handling support for users not using sudo to initialize the app.

--- src/utils/configUtils.js
+++ src/utils/configUtils.js
@@ -1,32 +1,31 @@
-import fs from 'fs';
-
+import fs from "fs";
 
 export function checkPermissionError(err) {
-    if (err.code === 'EACCES') {
-        console.error("Permission denied. Use 'sudo tracker init' to initialize the app.");
-        process.exit(1);
-    } else {
-        throw err;
-    }
+  if (err.code === "EACCES") {
+    console.error(
+      "Permission denied. Use 'sudo tracker init' to initialize the app."
+    );
+    process.exit(1);
+  } else {
+    throw err;
+  }
 }
-
 
 export function storeSudoUserInfo() {
-    return {
-        uid: process.env.SUDO_UID,
-        gid: process.env.SUDO_GID,
-        username: process.env.SUDO_USER
-    };
+  return {
+    uid: process.env.SUDO_UID,
+    gid: process.env.SUDO_GID,
+    username: process.env.SUDO_USER,
+  };
 }
 
-
 export function createUserOwnedDirs(dirs, uid, gid) {
-    for (let i = 0; i < dirs.length; i++) {
-        try {
-            fs.mkdirSync(dirs[i], { recursive: true });
-            fs.chownSync(dirs[i], parseInt(uid), parseInt(gid));
-        } catch (err) {
-            checkPermissionError(err);
-        }
+  for (let i = 0; i < dirs.length; i++) {
+    try {
+      fs.mkdirSync(dirs[i], { recursive: true });
+      fs.chownSync(dirs[i], parseInt(uid), parseInt(gid));
+    } catch (err) {
+      checkPermissionError(err);
     }
+  }
 }
